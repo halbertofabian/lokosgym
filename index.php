f69c12cfb25d3df373f691ba56186cdc305f5d14
Alberto Fabian <lf_alberto@hotmail.com>
2021-05-27 13:16:39 -0500

    foto

--- index.php
+++ index.php
@@ -1,38 +1,36 @@
 <?php
 ob_start();
-    require_once 'controlador/plantilla.controlador.php';
-    require_once 'controlador/categorias.controlador.php';
-    require_once 'controlador/productos.controlador.php';
-    require_once 'controlador/usuarios.controlador.php';
-    require_once 'controlador/ventas.controlador.php';
-    require_once 'controlador/clientes.controlador.php';
-   require_once 'controlador/membresias.controlador.php';
-   require_once 'controlador/cajas.controlador.php';
+require_once 'controlador/plantilla.controlador.php';
+require_once 'controlador/categorias.controlador.php';
+require_once 'controlador/productos.controlador.php';
+require_once 'controlador/usuarios.controlador.php';
+require_once 'controlador/ventas.controlador.php';
+require_once 'controlador/clientes.controlador.php';
+require_once 'controlador/membresias.controlador.php';
+require_once 'controlador/cajas.controlador.php';
 
 
-    //Modelos
+//Modelos
 
-    require_once 'modelo/categorias.modelo.php';
-    require_once 'modelo/productos.modelo.php';
-    require_once 'modelo/rutas.php';
-    require_once 'modelo/usuarios.modelo.php';
+require_once 'modelo/categorias.modelo.php';
+require_once 'modelo/productos.modelo.php';
+require_once 'modelo/rutas.php';
+require_once 'modelo/usuarios.modelo.php';
 
-    require_once 'modelo/softmor.moodelo.php';
-  
+require_once 'modelo/softmor.moodelo.php';
 
-    require_once 'modelo/ventas.modelo.php';
 
-    require_once 'modelo/clientes.modelo.php';
-    
-    require_once 'modelo/membresias.modelo.php';
-    require_once 'modelo/cajas.modelo.php';
+require_once 'modelo/ventas.modelo.php';
 
-    
+require_once 'modelo/clientes.modelo.php';
 
-    // Presentar la plantilla
+require_once 'modelo/membresias.modelo.php';
+require_once 'modelo/cajas.modelo.php';
 
-    $rutas = array();
 
-    
-    $startTemplate = PlantillaControlador::startTemplate();
 
+// Presentar la plantilla
+
+$rutas = array();
+
+$startTemplate = PlantillaControlador::startTemplate();
