8c8958684e9b5321d5de933035a8cdf043dff6d5
Alberto Fabian <lf_alberto@hotmail.com>
2021-10-13 10:13:23 -0500

    reportes

--- extensiones/tcpdf/pdf/reporte-compra.php
+++ extensiones/tcpdf/pdf/reporte-compra.php
@@ -23,9 +23,9 @@ if (isset($_GET['cps_id'])) {
 
     require_once('tcpdf_include.php');
 
-    // create new PDF document
-    $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
-
+   
+    $pageLayout = array(150, 150); //  or array($height, $width) 
+    $pdf = new TCPDF('P', 'mm', $pageLayout, true, 'UTF-8', false);
     // set document information
     $pdf->SetCreator(PDF_CREATOR);
     $pdf->SetAuthor('');
@@ -59,7 +59,7 @@ if (isset($_GET['cps_id'])) {
     $pdf->setPrintFooter(false);
     // Add a page
     // This method has several options, check the source code documentation for more information.
-    $pdf->AddPage('P');
+    $pdf->AddPage();
 
     // set text shadow effect
     // $pdf->setTextShadow(array('enabled' => true, 'depth_w' => 0.2, 'depth_h' => 0.2, 'color' => array(196, 196, 196), 'opacity' => 1, 'blend_mode' => 'Normal'));
